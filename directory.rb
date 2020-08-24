a6f3e029b7c902daa7f0518e4b01f70988438b3c
XavierDefontaine <xdefontaine@gmail.com>
2020-08-24 13:18:33 +0000

    made it interactive

--- directory.rb
+++ directory.rb
@@ -27,7 +27,27 @@ end
 def print_count(names)
 puts "Overall, we have #{names.count} great students"
 end
+
+def input_students
+puts "Please enter a name of the students"
+puts "To finish, just hit return twice"
+# creates an empty array
+students = []
+# get the first name
+name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+# return the array of students
+students
+end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_count(students)
